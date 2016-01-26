ac299202b719e4b640fc8346bf1bd84981bfdc79
Zeke Y <zeke@zystvan.com>
2016-01-25 19:02:27 -0500

    Move some code to different files + work on styling article cards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-#  helper_method :current_user
+  helper_method :current_user
   
-#  def current_user
-#    if session[:user_id] && User.find_by_id(session[:user_id])
-#      @current_user ||= User.find(session[:user_id])
-#    end
-#  end
-#  
-#  def require_logged_in
-#    redirect_to login_url unless current_user
-#  end
-#
-#  def require_logged_out
-#    redirect_to root_url unless !current_user
-#  end
+  def current_user
+    if session[:user_id] && User.find_by_id(session[:user_id])
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+  
+  def require_logged_in
+    redirect_to login_url unless current_user
+  end
+
+  def require_logged_out
+    redirect_to root_url unless !current_user
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,4 @@
 module ApplicationHelper
-  def current_user
-    if session[:user_id] && User.find_by_id(session[:user_id])
-      @current_user ||= User.find(session[:user_id])
-    end
-  end
-  
-  def require_logged_in
-    redirect_to login_url unless current_user
-  end
-
-  def require_logged_out
-    redirect_to root_url unless !current_user
-  end
-  
   def markdown(text)
     options = {
       filter_html: true,
